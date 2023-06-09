b69e51076b8389d9bd135722dd6d49f83ef8f2cf
Shokhrukh <shtojiboev@technounit.uz>
2023-06-09 13:41:04 +0500

    app  connect to GITHUB

---
+++ android/app/src/main/kotlin/com/example/flutter_natine_sip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_natine_sip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:sip_native/sip_native.dart';
+
+import 'home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SipNative.initPlugin().then((SipNativeSupport value) {
+    Fluttertoast.showToast(
+      msg: "Voip supported: ${value.isVoipSupported}\n" +
+          "SipManager supported: ${value.isSipManagerSupported}",
+      backgroundColor: value.isVoipSupported ? Colors.green : Colors.red,
+      toastLength: Toast.LENGTH_LONG,
+    );
+  });
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
